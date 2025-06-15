c777549f41aea459b175139c0f31e48eb090ca0e
Oluiscruz <oluiscruz25@gmail.com>
2025-06-14 21:13:56 -0300

    att-0.1

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,7 +23,7 @@ function Escopo() {
             {
                 title: "Turn Your Lights Down Low",
                 artist: "Bob Marley & The Wailers",
-                cover: "./assets/img/exodus.jpeg",
+                cover: "./assets/img/Exodus.jpeg",
                 file: "./assets/music/Turn-Your-Lights-Down-Low.mp3"
             },
 
@@ -37,7 +37,7 @@ function Escopo() {
             {
                 title: "Natural Mystic",
                 artist: "Bob Marley & The Wailers",
-                cover: "./assets/img/exodus.jpeg",
+                cover: "./assets/img/Exodus.jpeg",
                 file: "./assets/music/Natural-Mystic.mp3"
             }
         ];
